6b726c11703852b182d7b25a8177b90579df3392
GuyJoKing <653testing@gmail.com>
2019-11-04 16:20:02 +1100

    whitespace

--- proj/Blood.py
+++ proj/Blood.py
@@ -2,8 +2,8 @@
 # This is the list of blood bags to use as reference when getting blood by id
 bloodList = []
 def assignId(blood):
-	bloodList.append(blood)
-	return len(bloodList)-1
+    bloodList.append(blood)
+    return len(bloodList)-1
 
 class Blood(object):
     """docstring for Blood."""
@@ -14,7 +14,7 @@ class Blood(object):
         self._isVerified = False
         self._type = None
         self._storage = None
-		self._id = assignId(self)
+        self._id = assignId(self)
 
     def collectionDate(self):
         return self._collectionDate
