32e5643f94658c69c4c241873d44984c5213f7e2
waliy <waaaliy97@gmail.com>
2019-11-05 00:18:29 +1100

    storage

--- proj/Storage.py
+++ proj/Storage.py
@@ -1,15 +1,40 @@
 class Storage(object):
-    Blood = {blood:[]}
-    Fridge = {fridge:[Blood]}
-    Section = {section:[Fridge]}
-    Room = {room:[Section]}
-
+    types = ["O-", "O+", "B-", "B+", "A-", "A+", "AB-", "AB+"]
     def __init__(self):
-        self._capacity = []
-        self._capacity.append(Room)
+        self._inventory = []
+        self.inventory("Default")
+#TODO add blood quantity to each type
+
+
+    def inventory(self, desc):
+        self._inventory.append(self.room(desc))
+
+    def room(self, desc):
+        #list of blood class
+        #Fridge = {fridge:[Blood]}
+        #Section = {section:[Fridge]}
+        Room = {'name': desc, 'blood':[]}
+        for t in Storage.types:
+            Blood = {t:[]}
+            Room.get('blood').append(Blood)
+
+        return Room
+
+    def __str__(self):
+        str = "Rooms:\n"
+        for r in self._inventory:
+            str+=r.get('name')
+            str+=" Blood:"
+            for b in r.get('blood'):
+                str+="\n\t"
+                str+=b.__str__()
+            str+="\n"
+
+        return "{}".format(str)
 
 
 
-    def rooms(room):
-        self._capacity.append(Room)
-    def rooms()
+if __name__ == "__main__":
+    s = Storage()
+    s.inventory("Pakistan")
+    print(s)
